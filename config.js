52979dcdb6e2bb43db917de5fd15f4ab7db2d22b
Mas Kubo <makubo@amazon.com>
2026-04-28 13:32:00 -0700

    feat: Add Google Apps Script URL

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // Update scriptUrl after deploying the Google Apps Script backend
 const CONFIG = {
   // Google Apps Script Web App URL (set after deploying the script)
-  scriptUrl: "PASTE_YOUR_SCRIPT_URL_HERE",
+  scriptUrl: "https://script.google.com/macros/s/AKfycbzPOc0LrZkZwzqH-QRNMOm4P8fMBXVCClEpl7BvrDebV5Q1072bYCqoCeMtKOX9GnfK0A/exec",
 
   // Google Sheet ID (the long string in the sheet URL)
   sheetId: "PASTE_YOUR_SHEET_ID_HERE"
